615ebc438b064180b75ea7dd5da865aa7779a494
kmoran23 <kjpcmoran@me.com>
2021-03-24 15:29:22 -0600

    update timestamps page

--- spotitry/src/domains/main/Timestamps/index.js
+++ spotitry/src/domains/main/Timestamps/index.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
-import styles from '../Home/index.module.css'
+import React, {useState, Component } from 'react'
+import styles from '../Timestamps/index.module.css'
+import { connect } from 'react-redux'
+import { Table } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Timestamps = () => {
+    const {token} = props
+    console.log(token)
+    
     return(
-        <div className={styles.header}>This is the Timestamps Page</div>
+        <div className={styles.header}> Timestamps
+
+            <Table dark>
+
+            </Table>
+
+        </div>
+
     )
 }
 
