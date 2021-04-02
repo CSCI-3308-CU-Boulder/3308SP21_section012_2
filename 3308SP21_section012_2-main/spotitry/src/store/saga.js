5ae9122bef8f40693565f84ab3d85c1dcd787461
AdityaG123 <45723751+AdityaG123@users.noreply.github.com>
2021-04-02 12:39:10 -0600

    Delete 3308SP21_section012_2-main directory

--- 3308SP21_section012_2-main/spotitry/src/store/saga.js
+++
@@ -1,8 +0,0 @@
-import { all, call } from 'redux-saga/effects';
-import UserSaga from '../domains/main/redux/sagas.js'
-
-export default function* rootSaga() {
-    yield all([
-        call(UserSaga)
-    ]);
-}
