d9bb67a214200b67eb1696fe54385d5781e5b9c4
Sandy Griffin <59062958+sandygriffin@users.noreply.github.com>
2021-04-01 20:45:29 -0600

    updating index.js in timestamps

--- spotitry/src/domains/main/Timestamps/index.js
+++ spotitry/src/domains/main/Timestamps/index.js
@@ -25,12 +25,68 @@ const Timestamps = (props) => {
                 </InputGroup>
             </div>
 
-
-            <div>
+        <div>
+            <div className={styles.container}>
             <Card>
                 <CardImg top width="100%" src="" alt="Album Cover" />
                 <CardBody>
-                <CardTitle tag="h5">Song/Podcast title</CardTitle>
+                <CardTitle tag="h5">Song/Podcast title 1</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Artist Name</CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Album Name</CardSubtitle>
+                {/* <CardText>Artist Name</CardText>
+                <CardText>Album Name</CardText> */}
+                <CardText>Timestamps:</CardText>
+                <Button>Timestamp #1</Button>
+                </CardBody>
+            </Card>
+            </div>
+            <div className={styles.container}>
+            <Card>
+                <CardImg top width="100%" src="" alt="Album Cover" />
+                <CardBody>
+                <CardTitle tag="h5">Song/Podcast title 2</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Artist Name</CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Album Name</CardSubtitle>
+                {/* <CardText>Artist Name</CardText>
+                <CardText>Album Name</CardText> */}
+                <CardText>Timestamps:</CardText>
+                <Button>Timestamp #1</Button>
+                </CardBody>
+            </Card>
+            </div>
+            <div className={styles.container}>
+            <Card>
+                <CardImg top width="100%" src="" alt="Album Cover" />
+                <CardBody>
+                <CardTitle tag="h5">Song/Podcast title 3</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Artist Name</CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Album Name</CardSubtitle>
+                {/* <CardText>Artist Name</CardText>
+                <CardText>Album Name</CardText> */}
+                <CardText>Timestamps:</CardText>
+                <Button>Timestamp #1</Button>
+                </CardBody>
+            </Card>
+            </div>
+            <div className={styles.container}>
+            <Card>
+                <CardImg top width="100%" src="" alt="Album Cover" />
+                <CardBody>
+                <CardTitle tag="h5">Song/Podcast title 4</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Artist Name</CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Album Name</CardSubtitle>
+                {/* <CardText>Artist Name</CardText>
+                <CardText>Album Name</CardText> */}
+                <CardText>Timestamps:</CardText>
+                <Button>Timestamp #1</Button>
+                </CardBody>
+            </Card>
+            </div>
+            <div className={styles.container}>
+            <Card>
+                <CardImg top width="100%" src="" alt="Album Cover" />
+                <CardBody>
+                <CardTitle tag="h5">Song/Podcast title 5</CardTitle>
                 <CardSubtitle tag="h6" className="mb-2 text-muted">Artist Name</CardSubtitle>
                 <CardSubtitle tag="h6" className="mb-2 text-muted">Album Name</CardSubtitle>
                 {/* <CardText>Artist Name</CardText>
@@ -41,56 +97,10 @@ const Timestamps = (props) => {
             </Card>
             </div>
 
-            {/* <Table dark>
 
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>Title</th>
-                <th>Artist</th>
-                <th>Album</th>
-                <th>Timestamp(s)</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <th scope="row">1</th>
-                <td>Song 1 Title</td>
-                <td>Song 1 Artist</td>
-                <td>Song 1 Album</td>
-                <td>[[Saved Timestamps]] </td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Song 2 Title</td>
-                <td>Song 2 Artist</td>
-                <td>Song 2 Album</td>
-                <td>[[Saved Timestamps]] </td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td>Song 3 Title</td>
-                <td>Song 3 Artist</td>
-                <td>Song 3 Album</td>
-                <td>[[Saved Timestamps]] </td>
-                </tr>
-                <tr>
-                <th scope="row">4</th>
-                <td>Song 4 Title</td>
-                <td>Song 4 Artist</td>
-                <td>Song 4 Album</td>
-                <td>[[Saved Timestamps]] </td>
-                </tr>
-                <tr>
-                <th scope="row">5</th>
-                <td>Song 5 Title</td>
-                <td>Song 5 Artist</td>
-                <td>Song 5 Album</td>
-                <td>[[Saved Timestamps]] </td>
-                </tr>
-            </tbody>
+        </div>
 
-            </Table> */}
+
 
         </div>
 
