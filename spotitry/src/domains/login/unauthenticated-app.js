1d617e4038fdaa317628d4cd6e0a2f270be871df
kmoran23 <kjpcmoran@me.com>
2021-04-13 11:29:34 -0600

    styled buttons on login page and added card component

--- spotitry/src/domains/login/unauthenticated-app.js
+++ spotitry/src/domains/login/unauthenticated-app.js
@@ -1,7 +1,7 @@
 import styles from  './unauthenticated.module.css'
 import React, { useEffect, useState} from "react";
 import { spotifyLogo, clientId, redirectUri, hash, scopes, signUp, authEndpoint} from '../../utils/constants'
-import {Tabs, Tab, Button} from '@material-ui/core'
+import {Tabs, Tab, Button, Box} from '@material-ui/core'
 
 const UnauthenticatedApp = () => {
     const [accessToken, setAccessToken] = useState()
@@ -24,17 +24,21 @@ const UnauthenticatedApp = () => {
               <Tab label="Spotitry" />
             </Tabs>
         <header className={styles.AppHeader}>
+        
         <img src={spotifyLogo} className={styles.AppLogo} alt="Spotify Logo" />
-        {!accessToken && (
-          <Button className={styles.dumb} variant="contained" href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>
-            Login
-          </Button>
-        )}
-        {!accessToken && (
-          <Button variant="contained" href={`${signUp}`}>
-            Sign Up
-          </Button>
-        )}
+        <br></br>
+        <Box display="flex" justifyContent="space-between">
+          {!accessToken && (
+            <Button style={{height: '75px', width: '150px', margin: "20px"}} className={styles.dumb} variant="contained" href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>
+              Login
+            </Button>
+          )}
+          {!accessToken && (
+            <Button style={{height: '75px', width: '150px', margin: "20px"}} variant="contained" href={`${signUp}`}>
+              Sign Up
+            </Button>
+          )}
+        </Box>
         </header>
       </div>
     );
