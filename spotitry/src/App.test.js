db192e083c5f10975c5592730c824e0217d23ec6
ethanfwalters <ethan.f.walters@gmail.com>
2021-04-19 18:03:25 -0600

    adding the test cases

--- spotitry/src/App.test.js
+++ spotitry/src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// This testing is using the Jest frame work with the React Testing Library to use specialized tools
+
+import { App } from "./App";
+
+test('renders the app', () => {
+  const root = document.createElement("div");
+  render(<App />, root);
+
+});
+
+test('opening the login page', () =>{
+  const root = document.createElement("div");
+  render (<unauthenticated-app />, root);
+
+});
+
+test("search bar test", () =>{
+  const root = document.createElement("div");
+  render (<Discover/index.js />, root);
+  expect(root.querySelector(styles.searchBar)).toBe("Search Bar");
+});
+
+test("testing the time stamps", () =>{
+  const root = document.createElement("div");
+  render (<Timestamps/index.js />, root);
+  expect(root.isPrototypeOf('Timestamps'));
 });
